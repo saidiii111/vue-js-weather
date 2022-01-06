3f431ab599446dac7d7376877fc8e0084e65ab94
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2022-01-06 13:39:27 +0100

    ll start in the next step with Vue logic

--- vue-js-app/src/App.vue
+++ vue-js-app/src/App.vue
@@ -4,6 +4,16 @@
      <div class="search-box">
        <input type="text" class="search-bar" placeholder="Search..." />
      </div>
+     <div class="weather-wrap">
+       <div class="location-box">
+         <div class="location">hamburg, DE</div>
+         <div class="date">Thursday 06 Jan 2022</div>
+       </div>
+       <div class="weather-box">
+         <div class="temp">9°c</div>
+         <div class="weather">Rain</div>
+       </div>
+     </div>
    </main>
  </div>
 </template>
@@ -74,5 +84,47 @@ main {
   border-radius: 16px 0px 16px 0px;
 }
 
+.location-box .location {
+  color: #fff;
+  font-weight: 500;
+  font-size: 32px;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, .25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, .25);
+  background-color: rgba(255, 255, 255, .25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(255, 255, 255, .25);
+}
+
+.weather-box weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, .25);
+}
 
 </style>
