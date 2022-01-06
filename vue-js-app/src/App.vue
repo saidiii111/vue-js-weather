6a52b2ef945886d720e11c4f56835db850e84aef
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2022-01-06 13:22:17 +0100

    adding styles and more markup

--- vue-js-app/src/App.vue
+++ vue-js-app/src/App.vue
@@ -37,6 +37,42 @@ body {
   transition: .4s;
 }
 
+main {
+  min-height: 100vh;
+  padding: 25px;
+  
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, .25));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  widows: 100%;
+  padding:  15px;
+
+  color: blue;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+
+  box-shadow: 0px, 0px, 8px rgba(0, 0, 0, .25);
+  background-color: rgba(255, 255, 255, .5);
+  border-radius: 0px, 16px, 0px, 16px;
+  transition: .4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px, 0px, 16px rgba(0, 0, 0, .45);
+  background-color: rgba(255, 255, 255, .75);
+  border-radius: 16px 0px 16px 0px;
+}
 
 
 </style>
