bff3713b0b3f1efa9488ad24c44372f1ed631c02
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2022-01-06 13:08:16 +0100

    preparing npm and edit files to pass our weather app

---
+++ vue-js-app/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+ <div id="app">
+   <main>
+     <div class="search-box">
+       <input type="text" class="search-bar" placeholder="Search..." />
+     </div>
+   </main>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      api_key: '5d4147f3303193a9b1a6bf4cc0482630'
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'motserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: .4s;
+}
+
+
+
+</style>
